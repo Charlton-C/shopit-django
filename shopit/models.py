91810c31fbadc8b61ae0f779ed69b56d6e652c06
Charlton-C <user10219@protonmail.com>
2024-02-25 11:23:02 +0300

    Add User table to DB

--- shopit/models.py
+++ shopit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	firstname = models.CharField(max_length=200)
+	lastname = models.CharField(max_length=200)
+	username = models.CharField(max_length=250)
+	email = models.EmailField(max_length=150)
+	password = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.firstname + ' ' + self.surname + ' - ' + self.username
