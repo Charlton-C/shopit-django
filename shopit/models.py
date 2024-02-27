cb90ee6ba9084556f34da84d93e45cf15dd3a501
Charlton-C <user10219@protonmail.com>
2024-02-27 16:27:43 +0300

    Add product model

--- shopit/models.py
+++ shopit/models.py
@@ -9,3 +9,12 @@ class User(models.Model):
 
 	def __str__(self):
 		return self.firstname + ' ' + self.lastname + ' - ' + self.username
+
+class Product(models.Model):
+	productname = models.CharField(max_length=100)
+	productdescription = models.TextField(null=True, blank=True)
+	productprice = models.DecimalField(max_digits=12, decimal_places=2)
+	productimage = models.ImageField(upload_to='products/')
+
+	def __str__(self):
+		return self.productname + ' ' + str(self.productprice)
